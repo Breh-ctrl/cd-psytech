af3c329d52320d80b6955fbfe3f6aed88cb08750
pedro <115426902+psexex@users.noreply.github.com>
2026-04-23 20:02:27 -0300

    Add files via upload

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IonTitle, IonContent, IonButton, IonItem, IonInput, IonInputPasswordToggle} from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonTitle, IonContent,IonButton, IonItem, IonInput, IonInputPasswordToggle],
+  imports: [IonTitle, IonContent,IonButton, IonItem, IonInput, IonInputPasswordToggle, RouterLink],
 })
 export class HomePage {
   constructor() {}
