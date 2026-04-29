53a5dc09d5486fc6ded1ecf32c3f2cf89e950d89
pedro <115426902+psexex@users.noreply.github.com>
2026-04-29 20:35:00 -0300

    Add files via upload
    Adicionei um botão de senha junto ao CPF do paciente para a segurança ficar melhor

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { IonTitle, IonContent, IonButton, IonItem, IonInput} from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
+import { IonTabs } from '@ionic/angular/standalone';
+import { IonTabBar,IonIcon, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
   templateUrl: 'login.page.html',
   styleUrls: ['login.page.scss'],
-  imports: [IonTitle, IonContent,IonButton, IonItem, IonInput, RouterLink],
+  imports: [IonTitle, IonContent,IonButton, IonItem, IonInput, RouterLink, IonTabs, IonTabBar, IonIcon, IonLabel  ],
 })
 export class LoginPage {
   constructor() {}
