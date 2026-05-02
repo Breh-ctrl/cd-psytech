b2ed0a34057b4bab7f4bf300cb6cbe225df0f5cd
Brenda <raubackcastrobrenda@gmail.com>
2026-05-01 23:32:58 -0300

    css da pagina de login quase finalizado

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { IonTitle, IonContent, IonButton, IonItem, IonInput} from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
-import { IonTabs } from '@ionic/angular/standalone';
-import { IonTabBar,IonIcon, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
   templateUrl: 'login.page.html',
   styleUrls: ['login.page.scss'],
-  imports: [IonTitle, IonContent,IonButton, IonItem, IonInput, RouterLink, IonTabs, IonTabBar, IonIcon, IonLabel  ],
+  imports: [IonTitle, IonContent,IonButton, IonItem, IonInput, RouterLink],
 })
 export class LoginPage {
   constructor() {}
